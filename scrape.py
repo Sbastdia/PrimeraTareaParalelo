94858ff17934226a0eb05d224ebd0436608eceb6
Sbastdia <sbastdia@myuax.com>
2022-04-27 16:02:49 +0200

    prueba

---
+++ scrape.py
@@ -0,0 +1,11 @@
+import random
+from time import sleep
+
+urls = ["a.com", "b.com", "c.com", "d.com"]
+
+def scrape(url):
+    print("starting", url)
+    duration = round(random.random(), 3)
+    sleep(duration)
+    print("finished", url, "time taken:", duration, "seconds")
+    return url, duration
