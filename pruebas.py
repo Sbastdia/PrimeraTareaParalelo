54b76015f055404373ce87bcffc1135f4e6d4f38
Sbastdia <sbastdia@myuax.com>
2022-04-27 16:51:26 +0200

    prueba1

---
+++ pruebas.py
@@ -0,0 +1,27 @@
+import random
+from time import sleep
+from multiprocessing import Pool
+
+
+def scrape(url):
+    print("starting", url)
+    duration = round(random.random(), 3)
+    sleep(duration)
+    print("finished", url, "time taken:", duration, "seconds")
+    return url, duration
+
+
+urls = ["a.com", "b.com", "c.com", "d.com", "e.com", "f.com", "g.com", "h.com"]
+
+'''scrape ("a.com") # hecho por el proceso 1
+scrape ("b.com") # hecho por el proceso 2
+scrape ("c.com") # hecho por el proceso 3
+scrape ("d.com") # hecho por proceso 4'''
+
+if __name__ == "__main__":
+    pool = Pool(processes=2)
+    data = pool.map(scrape, urls)
+    pool.close()
+    print()
+    for row in data:
+        print(row)
